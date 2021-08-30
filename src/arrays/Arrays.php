49d0e9ed3250eb4b54d5b4f1f24bba653e89deed
unknown <you@example.com>
2021-08-30 11:26:42 +0000

    PHP array beatifier

--- src/arrays/Arrays.php
+++ src/arrays/Arrays.php
@@ -3,58 +3,78 @@ namespace arrays;
 
 class Arrays implements ArraysInterface
 {
-    public function repeatArrayValues(array $input): array{
-        $arr = [];
-        foreach ($input as $itm){
-            $i=0;
-            do {
-                $arr[] = $itm;      
-                $i++;
+    public function repeatArrayValues(array $input):
+        array
+        {
+            $arr = [];
+            foreach ($input as $itm)
+            {
+                $i = 0;
+                do
+                {
+                    $arr[] = $itm;
+                    $i++;
+                }
+                while ($i < $itm);
             }
-            while ($i < $itm);
+            return $arr;
         }
-        return $arr;
-    }
-    
-    public function getUniqueValue(array $input): int{
-        $uniq = [];
-        foreach ($input as $item){
-            $count=0;
-            foreach ($input as $itm){
-                if ($item == $itm){
-                    $count++;
+
+        public function getUniqueValue(array $input):
+            int
+            {
+                $uniq = [];
+                foreach ($input as $item)
+                {
+                    $count = 0;
+                    foreach ($input as $itm)
+                    {
+                        if ($item == $itm)
+                        {
+                            $count++;
+                        }
+                    }
+                    if ($count == 1)
+                    {
+                        $uniq[] = $item;
+                    }
+                }
+                if (count($uniq) > 0)
+                {
+                    return min($uniq);
+                }
+                return 0;
+            }
+
+            public function groupByTag(array $input):
+                array
+                {
+                    $uniqs = [];
+                    foreach ($input as $inp)
+                    {
+                        foreach ($inp['tags'] as $tag)
+                        {
+                            if (!in_array($tag, $uniqs))
+                            {
+                                $uniqs[] = $tag;
+                            }
+                        }
+                    }
+                    $result = [];
+                    foreach ($uniqs as $key => $uniq)
+                    {
+                        $names = [];
+                        foreach ($input as $inp)
+                        {
+                            if (in_array($uniq, $inp['tags']))
+                            {
+                                $names[] = $inp['name'];
+                            }
+                        }
+                        sort($names);
+                        $result[$uniq] = $names;
+                    }
+                    return $result;
                 }
             }
-            if($count == 1){
-                $uniq[] = $item;
-            }
-        }
-        if (count($uniq) > 0){
-            return min($uniq);
-        }
-        return 0;
-    }
-    
-    public function groupByTag(array $input): array{
-        $uniqs = [];
-        foreach($input as $inp){
-            foreach($inp['tags'] as $tag){
-                if (!in_array($tag, $uniqs)){
-                    $uniqs[] =  $tag;
-                }
-            }
-        }
-        $result = [];
-        foreach($uniqs as $key => $uniq){
-            $names = [];
-            foreach($input as $inp){
-                if(in_array($uniq, $inp['tags'])){
-                    $names[] = $inp['name'];
-                }
-            }
-            sort($names);
-            $result[$uniq] = $names;
-        }
-    return $result;       
-    }    
-}
+            
