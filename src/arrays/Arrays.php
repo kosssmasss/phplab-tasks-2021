30d50efd87e0c9cba05a354c1612225463955a8a
unknown <you@example.com>
2021-08-30 11:40:48 +0000

    one commit on yourBranch

---
+++ src/arrays/Arrays.php
@@ -0,0 +1,80 @@
+<?php
+namespace arrays;
+
+class Arrays implements ArraysInterface
+{
+    public function repeatArrayValues(array $input):
+        array
+        {
+            $arr = [];
+            foreach ($input as $itm)
+            {
+                $i = 0;
+                do
+                {
+                    $arr[] = $itm;
+                    $i++;
+                }
+                while ($i < $itm);
+            }
+            return $arr;
+        }
+
+        public function getUniqueValue(array $input):
+            int
+            {
+                $uniq = [];
+                foreach ($input as $item)
+                {
+                    $count = 0;
+                    foreach ($input as $itm)
+                    {
+                        if ($item == $itm)
+                        {
+                            $count++;
+                        }
+                    }
+                    if ($count == 1)
+                    {
+                        $uniq[] = $item;
+                    }
+                }
+                if (count($uniq) > 0)
+                {
+                    return min($uniq);
+                }
+                return 0;
+            }
+
+            public function groupByTag(array $input):
+                array
+                {
+                    $uniqs = [];
+                    foreach ($input as $inp)
+                    {
+                        foreach ($inp['tags'] as $tag)
+                        {
+                            if (!in_array($tag, $uniqs))
+                            {
+                                $uniqs[] = $tag;
+                            }
+                        }
+                    }
+                    $result = [];
+                    foreach ($uniqs as $key => $uniq)
+                    {
+                        $names = [];
+                        foreach ($input as $inp)
+                        {
+                            if (in_array($uniq, $inp['tags']))
+                            {
+                                $names[] = $inp['name'];
+                            }
+                        }
+                        sort($names);
+                        $result[$uniq] = $names;
+                    }
+                    return $result;
+                }
+            }
+            
