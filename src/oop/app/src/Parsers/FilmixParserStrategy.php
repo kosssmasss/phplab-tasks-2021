0727dac8a8f0e559bd2da0573db70425f8a63ba4
unknown <you@example.com>
2021-08-30 09:22:13 +0000

    OOP hometask

---
+++ src/oop/app/src/Parsers/FilmixParserStrategy.php
@@ -0,0 +1,25 @@
+<?php
+namespace src\oop\app\src\Parsers;
+
+Class FilmixParserStrategy implements ParserInterface {
+    public function parseContent(string $siteContent){
+
+        $siteContent = mb_convert_encoding($siteContent, "UTF-8", "windows-1251");
+        
+        $re = '/<h1 class="name" itemprop="name">([\w\s\d]+)<\/h1>/ui';
+        preg_match($re, $siteContent, $matches, PREG_OFFSET_CAPTURE, 0);
+        $title = $matches[0][0];
+
+        $re = '/<img src="(https:.*\.jpg)" class="poster poster-tooltip" itemprop="image" alt=".*" title=".*" loading="lazy"\/>/ui';
+        preg_match($re, $siteContent, $matches, PREG_OFFSET_CAPTURE, 0);
+        $poster = $matches[1][0];
+        
+        $re = '/<div\ class="about"\ itemprop="description"><div\ class="full-story">(.+)<\/div>/ui';
+        preg_match($re, $siteContent, $matches, PREG_OFFSET_CAPTURE, 0);
+        $description = $matches[0][0];
+        
+
+        $arr = ['title' => $title, 'poster'=> $poster, 'description' => $description];
+        return $arr;
+    }
+}
---
+++ src/oop/app/src/Transporters/CurlStrategy.php
@@ -0,0 +1,15 @@
+<?php
+namespace src\oop\app\src\Transporters;
+
+Class CurlStrategy implements TransportInterface {
+    public function getContent(string $url): string{
+        
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        $body = curl_exec($ch);
+        //var_dump(curl_getinfo($ch, CURLINFO_HEADER_OUT));
+        curl_close($ch);
+        return $body;
+    }
+}
